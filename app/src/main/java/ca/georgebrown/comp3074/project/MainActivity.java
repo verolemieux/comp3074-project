c88b197be67142bd7806dd423f8dc2efe8ce592c
verolemieux <veronyque.lemieux@gmail.com>
2019-10-23 19:06:47 -0400

    created project

---
+++ app/src/main/java/ca/georgebrown/comp3074/project/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.georgebrown.comp3074.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
