15bdb8fcf4f0de30f74c40674ae4a0fb6c8ca05d
sergiolombana <sergiolombana101@gmail.com>
2019-10-29 11:07:16 -0400

    Added intent between pages

--- app/src/main/java/ca/georgebrown/comp3074/project/Home.java
+++ app/src/main/java/ca/georgebrown/comp3074/project/Home.java
@@ -8,8 +8,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import ca.georgebrown.comp3074.project.Backpack.BackpacksActivity;
 import ca.georgebrown.comp3074.project.DatabaseAccess.UserDBAccess;
+import ca.georgebrown.comp3074.project.Event.EventsActivity;
 import ca.georgebrown.comp3074.project.Item.ItemsActivity;
+import ca.georgebrown.comp3074.project.Route.RoutesActivity;
 import ca.georgebrown.comp3074.project.User.User;
 
 public class Home extends AppCompatActivity {
@@ -25,6 +28,31 @@ public class Home extends AppCompatActivity {
         Button btnEvent = findViewById(R.id.btnEvents);
         Button btnRoute = findViewById(R.id.btnRoutes);
         Button btnItems = findViewById(R.id.btnItems);
+
+        btnBackpack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent backpackIntent = new Intent(view.getContext(), BackpacksActivity.class);
+                backpackIntent.putExtra("ValidateUser", validatedUser);
+                startActivity(backpackIntent);
+            }
+        });
+        btnRoute.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent routeIntent = new Intent(view.getContext(), RoutesActivity.class);
+                routeIntent.putExtra("ValidateUser", validatedUser);
+                startActivity(routeIntent);
+            }
+        });
+        btnEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent eventIntent = new Intent(view.getContext(), EventsActivity.class);
+                eventIntent.putExtra("ValidateUser",validatedUser );
+                startActivity(eventIntent);
+            }
+        });
 
         btnItems.setOnClickListener(new View.OnClickListener()
         {
