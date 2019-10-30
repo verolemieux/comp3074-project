cda37cd5cc6045d0c81b89f60352ff725dadf54c
Sharpmanjt <thibeau.jeremy@gmail.com>
2019-10-30 09:01:56 -0400

    Pushing what I was able to complete until now

--- app/src/main/java/ca/georgebrown/comp3074/project/Backpack/AddBackpackActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/project/Backpack/AddBackpackActivity.java
@@ -8,6 +8,7 @@ import ca.georgebrown.comp3074.project.R;
 
 public class AddBackpackActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/ca/georgebrown/comp3074/project/Backpack/BackpacksActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/project/Backpack/BackpacksActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import ca.georgebrown.comp3074.project.AboutActivity;
@@ -40,8 +41,33 @@ public class BackpacksActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent addBackpack = new Intent(v.getContext(), AddBackpackActivity.class);
                 addBackpack.putExtra("ValidatedUser", validatedUser);
-                startActivity(addBackpack);
+                addBackpack.putExtra("UserBackpacks", validatedUser.Backpack_List);
+                startActivityForResult(addBackpack, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1)
+        {
+            addBackpack = findViewById(R.id.btnAddBackpack);
+            validatedUser = (User)getIntent().getSerializableExtra("ValidatedUser");
+            userBackpacks = (ArrayList<Backpack>)getIntent().getSerializableExtra("UserBackpacks");
+            backpackList = findViewById(R.id.listBackpack);
+            adapter = new BackpackAdapter(this, R.layout.backpack_layout, userBackpacks);
+            backpackList.setAdapter(adapter);
+            addBackpack.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    Intent addBackpack = new Intent(v.getContext(), AddBackpackActivity.class);
+                    addBackpack.putExtra("ValidatedUser", validatedUser);
+                    startActivityForResult(addBackpack, 1);
+                }
+            });
+        }
     }
 }
