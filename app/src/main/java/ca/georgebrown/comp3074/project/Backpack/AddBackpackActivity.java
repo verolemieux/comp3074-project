69b2a93c1f566f1d6426936fa50c1c20a82b0c15
Sharpmanjt <thibeau.jeremy@gmail.com>
2019-10-30 12:31:13 -0400

    Pushing backpack changes

--- app/src/main/java/ca/georgebrown/comp3074/project/Backpack/AddBackpackActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/project/Backpack/AddBackpackActivity.java
@@ -2,16 +2,52 @@ package ca.georgebrown.comp3074.project.Backpack;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+
+import ca.georgebrown.comp3074.project.Item.Item;
+import ca.georgebrown.comp3074.project.Item.ItemAdapter;
 import ca.georgebrown.comp3074.project.R;
+import ca.georgebrown.comp3074.project.User.User;
 
 public class AddBackpackActivity extends AppCompatActivity {
 
-
+    ListView itemlist;
+    Button btnAdd;
+    User validatedUser;
+    ArrayList<Backpack> userBackpacks;
+    ArrayList<Item> items;
+    ItemAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_backpack);
+
+        validatedUser = (User)getIntent().getSerializableExtra("ValidatedUser");
+        userBackpacks = (ArrayList<Backpack>)getIntent().getSerializableExtra("UserBackpacks");
+        items = validatedUser.Item_List;
+        btnAdd = findViewById(R.id.btnAdd);
+        itemlist = findViewById(R.id.listItems);
+        adapter = new ItemAdapter(this, R.layout.item_layout, items);
+        itemlist.setAdapter(adapter);
+        btnAdd.setOnClickListener(new View.OnClickListener(){
+                @Override
+                public void onClick(View v)
+                {
+                    Intent addBackpackIntent = new Intent(v.getContext(), BackpacksActivity.class);
+
+
+                    setResult(1, addBackpackIntent);
+                    finish();
+                }
+
+            }
+
+        );
     }
 }
--- app/src/main/java/ca/georgebrown/comp3074/project/Item/ItemAdapter.java
+++ app/src/main/java/ca/georgebrown/comp3074/project/Item/ItemAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -29,9 +30,8 @@ public class ItemAdapter extends ArrayAdapter<Item> implements Serializable {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.item_layout, null);
         }
-
-        TextView txt = convertView.findViewById(R.id.txtItemLayout);
-        txt.setText(getItem(position).getItem_Name());
+            TextView txt = convertView.findViewById(R.id.txtItemLayout);
+            txt.setText(getItem(position).getItem_Name());
         return convertView;
     }
 }
