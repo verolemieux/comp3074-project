12afa5aec6800e4a6438ad9431bdaceacbe3100b
Sharpmanjt <thibeau.jeremy@gmail.com>
2019-11-12 21:48:59 -0500

    removing code no longer used

--- app/src/main/java/ca/georgebrown/comp3074/project/Item/ItemsActivity.java
+++ app/src/main/java/ca/georgebrown/comp3074/project/Item/ItemsActivity.java
@@ -80,103 +80,13 @@ public class ItemsActivity extends BaseActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == 1)
         {
-            ListView itemList = findViewById(R.id.listItems);
-            //itemList.getAdapter();
-            validatedUser = (User)data.getSerializableExtra("ValidatedUser");
-            ArrayList<Item> itemlist = (ArrayList<Item>) data.getSerializableExtra("ListItems");
-            ArrayList<Item> items = itemlist;
-            validatedUser.Item_List = items;
-            ItemAdapter adapter;
-            ImageButton addItem = findViewById(R.id.btnAddItem);
-            adapter = new ItemAdapter(this, R.layout.item_layout, items);
-            itemList.setAdapter(adapter);
-            itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, final int position, long l) {
-                    Intent editItemIntent = new Intent(view.getContext(), EditItemActivity.class);
-                    editItemIntent.putExtra("ValidatedUser", validatedUser);
-                    editItemIntent.putExtra("ListItems", validatedUser.Item_List);
-                    Item editItem = (Item)parent.getItemAtPosition(position);
-                    editItemIntent.putExtra("Item", editItem);
-                    startActivityForResult(editItemIntent, 2);
-                }
-            });
-
-            addItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent addItemIntent = new Intent(view.getContext(), AddItemActivity.class);
-                    addItemIntent.putExtra("ValidatedUser", validatedUser);
-                    addItemIntent.putExtra("ListItems", validatedUser.Item_List);
-                    startActivityForResult(addItemIntent, 1);
-                }
-            });
             items = itemTable.getItems(validatedUser.getEmail());
             adapter.notifyDataSetChanged();
         }
-        if(requestCode == 2)
-        {
+        if(requestCode == 2) {
             items = itemTable.getItems(validatedUser.getEmail());
             adapter.notifyDataSetChanged();
         }
-            ListView itemList = findViewById(R.id.listItems);
-            validatedUser = (User)data.getSerializableExtra("ValidatedUser");
-            ArrayList<Item> itemlist = (ArrayList<Item>) data.getSerializableExtra("ListItems");
-            Item editItem = (Item) data.getSerializableExtra("EditItem");
-            ArrayList<Item> items = itemlist;
-            validatedUser.Item_List = items;
-            ItemAdapter adapter;
-            ImageButton addItem = findViewById(R.id.btnAddItem);
-            if(data.getSerializableExtra("Function").equals("Edit"))
-            {
-                for(Item i : items)
-                {
-                    if(i.getItem_Id() == editItem.getItem_Id())
-                    {
-                        i.setItem_Name(editItem.getItem_Name());
-                        i.setDescription(editItem.getDescription());
-                    }
-                }
-            }
-            else
-            {
-                //Because for some reason remove object isn't working, below should be optimized
-                ArrayList<Item> item2 = new ArrayList<Item>();
-
-                for(Item i : items)
-                {
-                    if(i.getItem_Id() == editItem.getItem_Id())
-                    {
-                        continue;
-                    }
-                    item2.add(i);
-                }
-                items = item2;
-                validatedUser.Item_List = items;
-            }
-            adapter = new ItemAdapter(this, R.layout.item_layout, items);
-            itemList.setAdapter(adapter);
-            itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, final int position, long l) {
-                    Intent editItemIntent = new Intent(view.getContext(), EditItemActivity.class);
-                    editItemIntent.putExtra("ValidatedUser", validatedUser);
-                    editItemIntent.putExtra("ListItems", validatedUser.Item_List);
-                    Item editItem = (Item)parent.getItemAtPosition(position);
-                    editItemIntent.putExtra("Item", editItem);
-                    startActivityForResult(editItemIntent, 2);
-                }
-            });
-
-            addItem.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent addItemIntent = new Intent(view.getContext(), AddItemActivity.class);
-                    addItemIntent.putExtra("ValidatedUser", validatedUser);
-                    addItemIntent.putExtra("ListItems", validatedUser.Item_List);
-                    startActivityForResult(addItemIntent, 1);
-                }
-            });
         }
 
 
